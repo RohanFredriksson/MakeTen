e9ad12552dfd79fc15e075465f0f46989d9c3021
Rohan Fredriksson <rohan.fredriksson@gmail.com>
2023-07-02 23:43:57 +1000

    Started working on settings and info page.

--- src/screens/SettingsScreen.jsx
+++ src/screens/SettingsScreen.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
 import { View, Text, Settings } from 'react-native';
 import { getStyles } from './../styles/styles';
+import { getTheme } from './../styles/themes';
 
 const styles = getStyles();
+const theme = getTheme('dark');
 
 const SettingsScreen = (props) => {
 
-  console.log(Settings.get('spoiler'));
-
+  /*
   return (
     <View style={[styles.container, {backgroundColor: 'blue'}]}>
       <Text style={styles.text}>{(props.active ? "ACTIVE" : "INACTIVE")}</Text>
+    </View>
+  )
+  */
+
+  return (
+    <View style={[styles.container, {backgroundColor: theme.background}]}>
+      <View style={{position: 'absolute', width: 280, transform: [{translateY: -180}]}}>
+        <Text style={[styles.title, {color: theme.title, paddingBottom: 20}]}>Settings</Text>
+        <Text style={[styles.paragraph, {color: theme.paragraph, paddingBottom: 30}]}>Enter a 4 digit train code or{'\n'}swipe to explore</Text>
+        <Text style={[styles.title, {color: theme.title, paddingBottom: 20}]}>About</Text>
+        <Text style={[styles.paragraph, {color: theme.paragraph, paddingBottom: 30}]}>Enter a 4 digit train code or{'\n'}swipe to explore</Text>
+      </View>
     </View>
   )
   
