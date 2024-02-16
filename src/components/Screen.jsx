6ecdb36dfb634ee828ebde8397cd8cfe6f351a32
Rohan Fredriksson <rohan.fredriksson@gmail.com>
2024-02-16 13:23:11 +1100

    Fixed UI elements for landscape tablets.

--- src/components/Screen.jsx
+++ src/components/Screen.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { View, Dimensions, Platform } from 'react-native'
 //import { ScreenCornerRadius } from "react-native-screen-corner-radius"
 
-const ScreenCornerRadius = 10;
+const ScreenCornerRadius = 0;
 
 import { getStyles } from './../styles/styles';
 const styles = getStyles();
@@ -15,17 +15,26 @@ export default class Screen extends React.Component {
 
   render() {
 
+    const width = Dimensions.get('window').width;
+    const height = Dimensions.get('window').height;
+
     const children = React.Children.toArray(this.props.children).map((child) => {
       return React.cloneElement(child, {active: ('active' in this.props ? this.props.active : false), left: this.props.left, right: this.props.right});
     });
 
+    // If we need to change the positioning of elements.
+    let top = 0;
+
+    // Pixel Tablet has a bar at the bottom that blocks.
+    if (Platform.OS === 'android' && height / width  < 1.6) {top = -60;}
+
     return (
       <View 
         style={[styles.shadow, {
-          width: Dimensions.get('window').width, 
-          height: Dimensions.get('window').height, 
+          width: width, 
+          height: height,
           borderRadius: ScreenCornerRadius,
-          top: Platform.OS === 'ios' ? 0 : -10
+          top: top
         }]}
       >
         <View style={{width: '100%', height: '100%', borderRadius: ScreenCornerRadius, overflow: 'hidden',}}>
--- src/screens/CameraScreen.jsx
+++ src/screens/CameraScreen.jsx
@@ -98,19 +98,22 @@ const CameraScreen = (props) => {
   }
 
   const CameraBorder = () => {
+
+    const length = Math.min(0.75 * width, 0.65 * height);
+    const arm = Math.min(0.2 * width, 0.09 * height);
   
     return (
       <View style={{width: width, height: height, flex: 1}}>
-        <View style={{position: 'absolute', top: 0.5 * (height - width), left: 0.125 * width}}>
-          <View style={{position: 'relative', width: 0.75 * width, height: 0.75 * width}}>
-            <Animated.View style={[borderStyle, {position: 'absolute', top:    -5, left:  -5, width: 7.5, height: 0.2 * width}]}></Animated.View>
-            <Animated.View style={[borderStyle, {position: 'absolute', top:    -5, left:  -5, width: 0.2 * width, height: 7.5}]}></Animated.View>
-            <Animated.View style={[borderStyle, {position: 'absolute', top:    -5, right: -5, width: 7.5, height: 0.2 * width}]}></Animated.View>
-            <Animated.View style={[borderStyle, {position: 'absolute', top:    -5, right: -5, width: 0.2 * width, height: 7.5}]}></Animated.View>
-            <Animated.View style={[borderStyle, {position: 'absolute', bottom: -5, left:  -5, width: 7.5, height: 0.2 * width}]}></Animated.View>
-            <Animated.View style={[borderStyle, {position: 'absolute', bottom: -5, left:  -5, width: 0.2 * width, height: 7.5}]}></Animated.View>
-            <Animated.View style={[borderStyle, {position: 'absolute', bottom: -5, right: -5, width: 7.5, height: 0.2 * width}]}></Animated.View>
-            <Animated.View style={[borderStyle, {position: 'absolute', bottom: -5, right: -5, width: 0.2 * width, height: 7.5}]}></Animated.View>
+        <View style={{position: 'absolute', top: (0.65 * height < 0.75 * width ? 0.4 : 0.5) * (height - length), left: 0.5 * (width - length)}}>
+          <View style={{position: 'relative', width: length, height: length}}>
+            <Animated.View style={[borderStyle, {position: 'absolute', top:    -5, left:  -5, width: 7.5, height: arm}]}></Animated.View>
+            <Animated.View style={[borderStyle, {position: 'absolute', top:    -5, left:  -5, width: arm, height: 7.5}]}></Animated.View>
+            <Animated.View style={[borderStyle, {position: 'absolute', top:    -5, right: -5, width: 7.5, height: arm}]}></Animated.View>
+            <Animated.View style={[borderStyle, {position: 'absolute', top:    -5, right: -5, width: arm, height: 7.5}]}></Animated.View>
+            <Animated.View style={[borderStyle, {position: 'absolute', bottom: -5, left:  -5, width: 7.5, height: arm}]}></Animated.View>
+            <Animated.View style={[borderStyle, {position: 'absolute', bottom: -5, left:  -5, width: arm, height: 7.5}]}></Animated.View>
+            <Animated.View style={[borderStyle, {position: 'absolute', bottom: -5, right: -5, width: 7.5, height: arm}]}></Animated.View>
+            <Animated.View style={[borderStyle, {position: 'absolute', bottom: -5, right: -5, width: arm, height: 7.5}]}></Animated.View>
           </View>
         </View>
       </View>
