55082112357a8f694fc1323c3e1024bade1b5aa1
Rohan Fredriksson <rohan.fredriksson@gmail.com>
2024-02-19 15:42:50 +1100

    Changed namespace again

--- android/app/src/main/java/com/maketen/MainActivity.kt
+++ android/app/src/main/java/com/maketen/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.rojuanodev.maketen
+package com.rohanfredriksson.maketen
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/maketen/MainApplication.kt
+++ android/app/src/main/java/com/maketen/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.rojuanodev.maketen
+package com.rohanfredriksson.maketen
 
 import android.app.Application
 import com.facebook.react.PackageList
